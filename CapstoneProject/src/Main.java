60d4fb4f51fd440ef41a14f2230b321599274280
Quang Le <qle896@student.fuhsd.org>
2022-05-02 11:13:14 -0700

    Update Main.java

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		JFrame window = new JFrame("Banner");
 
 		window.setBounds(300, 300, 200, 150);
@@ -15,7 +16,18 @@ public class Main {
 		window.add(panel);
 
 		window.setVisible(true);
+        */
+		
+		JFrame w = new JFrame("Simple Window");
+		w.setBounds(100, 100, 640, 480);
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Window panel = new Window();
+		w.addKeyListener(panel);
+		w.add(panel);
+		w.setResizable(true);
+		w.setVisible(true);
 
+		panel.run();
 
 	}
 
