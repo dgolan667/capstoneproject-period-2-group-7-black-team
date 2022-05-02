6223085cd885495740634917105e783308bdf467
ychen194 <ychen194@student.fuhsd.org>
2022-05-02 11:11:07 -0700

    window

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -1,8 +1,21 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame window = new JFrame("Banner");
+
+		window.setBounds(300, 300, 200, 150);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		DrawingSurface panel = new DrawingSurface();
+		panel.setBackground(Color.WHITE);  // the default color is light gray
+		window.add(panel);
+
+		window.setVisible(true);
+
 
 	}
 
