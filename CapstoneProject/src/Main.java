64bb182e8b0fb9bcb45de691e872232a4ca2da30
ychen194 <ychen194@student.fuhsd.org>
2022-05-02 11:14:24 -0700

    Update Main.java

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -1,21 +1,8 @@
-import java.awt.Color;
-
-import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame window = new JFrame("Banner");
-
-		window.setBounds(300, 300, 200, 150);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		DrawingSurface panel = new DrawingSurface();
-		panel.setBackground(Color.WHITE);  // the default color is light gray
-		window.add(panel);
-
-		window.setVisible(true);
-
+		// TODO Auto-generated method stub
 
 	}
 
