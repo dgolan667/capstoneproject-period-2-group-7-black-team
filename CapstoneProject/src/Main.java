cfef95a31617b58d18ad515ade510bfd22232521
Quang Le <lequangdai08@gmail.com>
2022-05-03 10:58:52 -0700

    create a window

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -1,10 +1,23 @@
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		JFrame window = new JFrame("Flappy Bird");
+		window.setBounds(100, 100, 600, 400);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+		/*
+		Window panel = new Window();
+		panel.setBackground(Color.CYAN);  // the background color is cyan
+		window.add(panel);
+        */
 		
-		
+		window.setVisible(true);
 
 	}
 
