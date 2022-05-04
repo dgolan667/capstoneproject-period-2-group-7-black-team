afc9b21738a069c2677122aa9b4ca2e6b7c97248
ychen194 <ychen194@student.fuhsd.org>
2022-05-04 09:34:19 -0700

    window

--- CapstoneProject/src/Main.java
+++ CapstoneProject/src/Main.java
@@ -3,23 +3,31 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import java.awt.Component;
+
+
 public class Main extends JPanel {
 	
 	public Main () {
 		super();
-		setBackground(Color.CYAN);
+		Color LBLUE = new Color (51, 153, 255); 
+		setBackground(LBLUE);
 	}
 	
 	public static void main(String[] args) {
-		JFrame w = new JFrame("Window");
-		w.setBounds(100, 100, 640, 480);
-		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Main panel = new Main();
-		w.add(panel);
-		w.setResizable(true);
-		w.setVisible(true);
-		
-		
+
+		JFrame window = new JFrame("Flappy Bird");
+		window.setBounds(100, 100, 600, 400);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Main panel = new Main();  
+        window.add(panel);
+		/*
+		Window panel = new Window();
+		panel.setBackground(Color.CYAN);  // the background color is cyan
+		window.add(panel);
+        */
+		window.setResizable(true);
+		window.setVisible(true);
 
 	}
 
