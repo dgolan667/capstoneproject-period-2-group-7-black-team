8bc758ae68fc903eff66e2504e1c5be99b2612da
DongKyu(Kevin) Lee <90276190+kevinink11@users.noreply.github.com>
2022-05-05 10:48:44 -0700

    Pipe and Extend Pipe class(PipeMovement)

---
+++ CapstoneProject/src/Pipe.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+
+import javax.swing.ImageIcon;
+
+public class Pipe extends PipeMovement{
+
+	private int x,y;
+	private int width, height;
+	private Image image;
+	
+	public Pipe(int x, int y) {
+		this("Pipe.png",x,y,40,50);
+	}
+
+	public Pipe(String filename, int x, int y, int w, int h) {
+		
+		this((new ImageIcon(filename)).getImage(),x,y,w,h);
+	}
+
+	public Pipe(Image img, int x, int y, int w, int h) {
+		
+		super(img, x, y, w, h);
+	}	
+	
+	
+}
