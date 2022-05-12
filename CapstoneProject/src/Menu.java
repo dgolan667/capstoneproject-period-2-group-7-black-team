75db8ba2a174eb33765c306cc7eb6aae4c62ac13
DongKyu(Kevin) Lee <90276190+kevinink11@users.noreply.github.com>
2022-05-11 22:25:48 -0700

    add the button

--- CapstoneProject/src/Menu.java
+++ CapstoneProject/src/Menu.java
@@ -1,14 +1,60 @@
 import javax.swing.JButton;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
 
-public class Menu {
+import java.awt.event.*;
+
+
+public class Menu extends  JPanel implements ActionListener{
     // FIELDS
 	private int score;
 	private JButton button1, button2;
 	
+	JLabel guest = new JLabel("Guest");
+	
+	
+	
 	public Menu() {
 		score = 0;
 		button1 = new JButton("Restart");
+		button1.addActionListener(this);
 		button2 = new JButton("Character");	
+		button2.addActionListener(this);
+		
+		Container c = getRootPane();
+		c.setLayout(new FlowLayout());
+		c.add(button1);
+		c.add(button2);
+		
+		
+	}
+/*
+ * JButton button = new JButton("Play the Game!");
+		button.addActionListener(this);
+		p.add(button);
+		add(p);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		w.changePanel("2");
+	}*/
+
+
+
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==(button1))
+		{
+					
+		}
+		else if(e.getSource()==(button2)) {
+			
+		}
 	}
 	
 	
