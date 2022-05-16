6f9387303f90ad487bff1b0800304939e927d96e
DongKyu(Kevin) Lee <90276190+kevinink11@users.noreply.github.com>
2022-05-16 11:08:57 -0700

    delete character button in starting menu

--- CapstoneProject/src/StartingMenu.java
+++ CapstoneProject/src/StartingMenu.java
@@ -11,7 +11,6 @@ public class StartingMenu extends JPanel implements ActionListener {
 			
 			this.a = a;
 			JPanel p = new JPanel();
-			JPanel p1 = new JPanel();
 			
 			
 			p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
@@ -21,15 +20,6 @@ public class StartingMenu extends JPanel implements ActionListener {
 			button.addActionListener(this);
 			p.add(button);
 			add(p);
-			
-			
-			p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
-			p.add(Box.createVerticalStrut(300));
-			
-			JButton button1 = new JButton("Character");
-			button1.addActionListener(this);
-			p1.add(button1);
-			add(p1);
 			
 		}
 	
